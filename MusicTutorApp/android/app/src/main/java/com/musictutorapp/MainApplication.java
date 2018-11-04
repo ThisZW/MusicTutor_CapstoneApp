58e1a39a43451ee4dce2b7711d880313a5f7478e
sergiolemus <sergiolemusofficial@gmail.com>
2018-11-03 20:10:02 -0400

    Includes test module

--- MusicTutorApp/android/app/src/main/java/com/musictutorapp/MainApplication.java
+++ MusicTutorApp/android/app/src/main/java/com/musictutorapp/MainApplication.java
@@ -9,6 +9,8 @@ import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 
+import com.musictutorapp.module.AudioPackage;
+
 import java.util.Arrays;
 import java.util.List;
 
@@ -23,8 +25,9 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-            new OrientationPackage()
+            new MainReactPackage(),
+            new OrientationPackage(),
+            new AudioPackage()
       );
     }
 
